bfc27dc4f8c9fdda6f1dc647f2cf8faf0ebccfea
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-12 15:41:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,9 +3,14 @@
 #include <cmath>
 using namespace std;
 
-int funzione(float numeri[], float dimensione){
+int funzione( float numeri[], int dimensione){
+    float max=0-100000;
     for (int i=0; i<dimensione; i++){
         cin >> numeri[i];
     }
+    for(int i=0;i<dimensione;i++){
+       if (numeri[i]>max) max=numeri[i];
+    }
+    return max;
 }
 
