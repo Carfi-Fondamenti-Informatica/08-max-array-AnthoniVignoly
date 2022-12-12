9d3d4c15b4181410a88deeb38cc9cbf63e21e0d9
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-12 15:40:35 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,5 +1,9 @@
+//
+// Created by antho on 12/12/2022.
+//
+
 #ifndef ESERCITAZIONE_8_LIB_H
 #define ESERCITAZIONE_8_LIB_H
-int funzione(float numeri[], float dimensione);
+int funzione(float numeri[], int dimensione);
 #endif //ESERCITAZIONE_8_LIB_H
 
