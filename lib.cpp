5e9f8f83f21a6a1cc2c401759fdbd860b8b1e3c7
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-12 15:13:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int funzione(float numeri[], float dimensione){
+    for (int i=0; i<dimensione; i++){
+        cin >> numeri[i];
+    }
+}
 
