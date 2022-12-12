9b441f365b92c5c6c0ec5b55ff5b696b08265cec
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-12 15:12:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include 
+using namespace std;
 
 int main(){
+    int dimensione;
+    cin >> dimensione;
+    float vettore[dimensione];
+    cout << funzione(vettore,dimensione) << endl;
   return 0;
 }
